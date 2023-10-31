47872eee8c3adf8f1760335b71d4dc4c10051f3a
Joel Erni <joel.erni@hispeed.ch>
2023-10-31 14:46:27 +0100

    main update

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ topic_pub = b"TestValue"
 def mqtt(data):
     c = MQTTClient(
         client_id='0',
-        user = 'Jeppy',
-        server = mqtt_server,
-        password = 'aio_eQrZ276SkfnmhgxGw1PPHtxgqK4C'
+        #user = 'Jeppy',
+        server = "185.80.65.82:1883",
+        #password = 'aio_hmHz68Df4iapfQ67MxctYnMLaihZ'
     )
     c.connect()
-    c.publish('Jeppy/feeds/test', b'{}'.format(data))
+    c.publish('hello/topic', b'{}'.format(data))
     c.disconnect()
 
 def main():
@@ -36,3 +36,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
