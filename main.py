5c2517dccbf7d3cf75a9cd717f8acd9d867d1464
Joel Pascal Erni <102746806+JoelErni@users.noreply.github.com>
2023-11-05 16:05:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,24 @@ import temperature
 import gc
 gc.collect()
 
+#Config_File
+config_file = json.load(open('config.json'))
 
 #MQTT
-mqtt_server = "io.adafruit.com"
+mqtt_server = config_file['mqtt']['server']
 client_id = ubinascii.hexlify(machine.unique_id())
 topic_pub = b"TestValue"
 
 
 def mqtt(data):
     c = MQTTClient(
-        client_id='0',
-        #user = 'Jeppy',
-        server = "192.168.139.80",
+        client_id = config_file['mqtt']['client_id'],
+        #user = 'config_file['mqtt']['user'],
+        server = config_file['mqtt']['server'],
         #password = 'aio_hmHz68Df4iapfQ67MxctYnMLaihZ'
     )
     c.connect()
-    c.publish('hello/topic', b'{}'.format(data))
+    c.publish(config_file['mqtt']['topic'], b'{}'.format(data))
     c.disconnect()
 
 def main():
